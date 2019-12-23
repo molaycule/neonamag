8c9ac5f2a67c7976a2ccdaa2234c2fe9faeba9dd
moodele <moh.agboola@gmail.com>
2019-12-23 07:09:10 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Showcase from './components/Showcase';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Fashion from './components/Fashion';
+import Lifestyle from './components/Lifestyle';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Showcase />
+      <Navbar />
+      <About />
+      <Fashion />
+      <Lifestyle />
     </div>
   );
 }
